550038c20d8451416c6764d214b33e752ba07269
Matvey <mnf89@mail.ru>
2020-02-21 22:53:27 +0300

    2d array

---
+++ 2D Array.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+let arr = [[1, 1, 1, 0, 0, 0],
+           [0, 1, 0, 0, 0, 0],
+           [1, 1, 1, 0, 0, 0],
+           [0, 0, 2, 4, 4, 0],
+           [0, 0, 0, 2, 0, 0],
+           [0, 0, 1, 2, 4, 0]]
+
+let arr2 = [[-9, -9, -9, 1, 1, 1],
+            [0, -9, 0, 4, 3, 2],
+            [-9, -9, -9, 1, 2, 3],
+            [0, 0, 8, 6, 6, 0],
+            [0, 0, 0, -2, 0, 0],
+            [0, 0, 1, 2, 4, 0]]
+
+func hourglassSum(arr: [[Int]]) -> Int {
+    var arrayOfSums = [Int]()
+    for i in 0..<4 {
+        for j in 0..<4 {
+            let sum = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
+            arrayOfSums.append(sum)
+        }
+    }
+    return arrayOfSums.max() ?? 0
+}
+
+print(hourglassSum(arr: arr2))
